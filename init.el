bf09d72f0085464d2b3ba214c68d2d51c33d28e3
Bartholomäus Kaminski <bartholomaeus.kaminski@obi.de>
2019-02-06 19:05:15 +0100

    Fix package-init warning
    - Add more base config

--- init.el
+++ init.el
@@ -11,7 +11,7 @@
 			 ("melpa" . "https://melpa.org/packages/")
 			 ("org"   . "https://orgmode.org/elpa/" )))
 
-(package-initialize)
+(unless package--initialized (package-initialize t))
 
 ;;; Bootstrapping use-package
 (unless (package-installed-p 'use-package)
@@ -30,7 +30,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default))
- '(package-selected-packages '(magit spacemacs-theme use-package)))
+ '(package-selected-packages '(pretty-mode magit spacemacs-theme use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
